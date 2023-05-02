18808a8641ac90298881dddccd0b23cbe955dcbd
ridhhivyas2001@gmail.com <ridhhivyas2001@gmail.com>
2023-05-02 19:06:18 +0530

    added files

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+export default function Home() {
+  return (
+    <div>
+       <Link to="/NasaPhoto">See into the stars</Link>
+    </div>
+  )
+}
---
+++ src/components/NasaPhoto.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+
+export default function NasaPhoto() {
+  
+      const [photoData, setPhotoData]= useState(null);
+
+useEffect(() => {
+
+  fetchPhoto();
+  async function fetchPhoto(){
+    const res=await fetch(
+        'https://api.nasa.gov/planetary/apod?api_key=2CdSP0Zjs8cdbWQX6ucwX3eqYfjzJMOuVibjkne2'
+    );
+    console.log(res)
+    const data= await res.json();
+    setPhotoData(data);
+}
+},[]);
+if(!photoData) return <div  />;
+
+return(
+
+    <div>
+        <img
+        src={photoData.url}
+        alt={photoData.title}
+        />
+        <h1>{photoData.title}</h1>
+        <p>{photoData.date}</p>
+        <p>{photoData.explanation}</p>
+        
+        
+    </div>
+
+  )
+}
